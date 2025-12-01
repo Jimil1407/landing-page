9885fa4f485b849b075bfc807f08a2a55d36175c
Jimil1407 <jimildigaswala@gmail.com>
2025-12-01 11:50:03 +0000

    added content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("hello");
+document.getElementById('changeColorBtn').addEventListener('click', function() {
+    const colors = ['#f4f4f4', '#ffebcd', '#d3f8e2', '#f9c6c9', '#c3bef0'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+});
+
